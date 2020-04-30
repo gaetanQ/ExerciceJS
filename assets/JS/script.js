0727c02e8289b86b519c652dd8dc537bb3600020
gaetanQ <querolle.gaetan@gmail.com>
2020-04-30 13:26:51 +0200

    exercice_js

---
+++ assets/JS/script.js
@@ -0,0 +1,8 @@
+function controle () {
+    var number8 = document.getElementById("number8").value;
+
+    var number9 = document.getElementById("number9").value;
+
+    alert('Reste : ' + number8 % number9);
+}
+
