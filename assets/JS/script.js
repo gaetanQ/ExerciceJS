d0342a24e58531902dd94c8fedcb46a3363e5863
gaetanQ <querolle.gaetan@gmail.com>
2020-05-04 16:55:13 +0200

    04.05

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,8 +1,46 @@
+/*exercice 5*/
+
 function controle () {
-    var number8 = document.getElementById("number8").value;
+    var myVar = document.getElementById("number1").value;
 
-    var number9 = document.getElementById("number9").value;
+    var myVar2 = document.getElementById("number2").value;
 
-    alert('Reste : ' + number8 % number9);
+    var myResult = (myVar % myVar2);
+
+    alert(myResult);
+}
+/* exercice 6*/
+function controle() {
+
+    var myVarShoesSize = parseInt(document.getElementById("shoesSize").value);
+
+    var myVarBirthDate = parseInt(document.getElementById("birthDate").value);
+
+    var myResult = ((((myVarShoesSize*2)+5)*50)-myVarBirthDate+1769);
+
+    
+   alert(myResult);
 }
 
+
+/*exercice 7*/
+
+function check() {
+     var myNumber = parseInt(document.getElementById("age").value);
+
+      if (myNumber>=18) {
+                    alert("Vous êtes majeur");
+      } 
+      
+      else  {
+          alert("vous êtes mineur");
+      }
+ 
+}
+/*Partie2*/
+
+/*Exercice 2*/
+function key(this.value){
+var
+    
+}
